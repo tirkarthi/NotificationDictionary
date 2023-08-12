314a2a6aeeff9958359af90b20a1edd1457315d2
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2023-08-12 14:00:22 +0530

    Add support to have meaning inline for words of plurals and different tense. (#31)
    * Add support to have meaning inline for words of plurals and different tense.

--- app/src/main/java/com/xtreak/notificationdictionary/ProcessTextActivity.kt
+++ app/src/main/java/com/xtreak/notificationdictionary/ProcessTextActivity.kt
@@ -44,6 +44,9 @@ class ProcessTextActivity : AppCompatActivity() {
             var meaning: Word?
             try {
                 meaning = dao.getMeaningsByWord(word, 1)
+                if (meaning != null) {
+                    resolveRedirectMeaning(listOf(meaning) as List<Word>, dao)
+                }
             } catch (e: Exception) {
                 Sentry.captureException(e)
                 meaning = Word(
