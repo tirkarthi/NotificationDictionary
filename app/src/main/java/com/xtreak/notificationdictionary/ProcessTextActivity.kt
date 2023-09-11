979fb366130450649cd835d7a1e48ccec40b2224
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2023-09-11 12:42:13 +0530

    Add intent to receive selected text through share menu.

--- app/src/main/java/com/xtreak/notificationdictionary/ProcessTextActivity.kt
+++ app/src/main/java/com/xtreak/notificationdictionary/ProcessTextActivity.kt
@@ -31,8 +31,13 @@ class ProcessTextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val word: String
         val context = applicationContext
-        val word = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString().lowercase()
+        if (intent?.action == Intent.ACTION_SEND && intent.type == "text/plain") {
+            word = intent.getCharSequenceExtra(Intent.EXTRA_TEXT).toString().lowercase()
+        } else {
+            word = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString().lowercase()
+        }
 
         val executor = Executors.newSingleThreadExecutor()
         var definition = "No meaning found"
